b82b2c6096a38a28fd2354bbea35ef438fe7745f
Dima Shmatko <ponyrunrun1925@gmail.com>
2022-09-19 19:43:05 -0700

    refactored script

--- index.js
+++ index.js
@@ -11,43 +11,72 @@ const app = express();
 
 app.use(cors({ credentials: true, origin: "*" }));
 
-app.set('json spaces', 2)
+
+app.set("json spaces", 2);
+
+app.use(express.static("public"))
 
 const config = {
   headers: { "Content-type": "application/x-www-form-urlencoded" },
 };
 
-const body = {
+const authBody = {
   grant_type: "client_credentials",
   client_id: process.env.CLIENT_ID,
   client_secret: process.env.CLIENT_SECRET,
 };
+
+const authMappedData = Object.keys(authBody)
+  .map(function (key) {
+    return encodeURIComponent(key) + "=" + encodeURIComponent(authBody[key]);
+  })
+  .join("&");
 
 app.get("/", (req, res) => {
   axios({
     method: "post",
     url: "https://carrier.superdispatch.com/oauth/token/",
     withCredentials: true,
-    data: Object.keys(body)
-      .map(function (key) {
-        return encodeURIComponent(key) + "=" + encodeURIComponent(body[key]);
-      })
-      .join("&"),
+    data: authMappedData,
     config,
   })
-    .then((response) => {
+    .then((authRes) => {
       const config = {
         headers: {
-          Authorization: `Bearer ${response.data.access_token}`,
+          Authorization: `Bearer ${authRes.data.access_token}`,
         },
       };
 
+      const dataArr = [];
+      let count = 4;
+
+      function getPaginationData(nextUrl) {
+        if (!count) {
+          res.json(dataArr);
+          return;
+        }
+        count -= 1;
+        return axios
+          .get(nextUrl, config)
+          .then((res1) => {
+            dataArr.push(...res1.data.data);
+            return getPaginationData(res1.data.pagination.next);
+          })
+          .catch(() => {
+            res.json(dataArr);
+            return;
+          });
+      }
+
       axios
         .get(`${API_BASE_URL}/orders`, config)
-        .then((response) => {
-          res.json(response.data);
+        .then((ordersRes) => {
+          const nextUrl = ordersRes.data.pagination.next;
+          const data = ordersRes.data.data;
+          dataArr.push(...data);
+          getPaginationData(nextUrl);
         })
-        .catch((error) => {
+        .catch(() => {
           res.sendStatus(500);
         });
     })
@@ -55,6 +84,5 @@ app.get("/", (req, res) => {
       return err;
     });
 });
-
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
